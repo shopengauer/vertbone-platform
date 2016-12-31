21c943cd7a292e3d849fa4c29562d27cd6132476
root <localducks@gmail.com>
2016-12-31 17:28:57 +0300

    Initial Vertbone commit

---
+++ vertbone-entry/src/main/java/com/vertbone/entry/VertboneEntryApplication.java
@@ -0,0 +1,12 @@
+package com.vertbone.entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertboneEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertboneEntryApplication.class, args);
+	}
+}
---
+++ vertbone-entry/src/main/java/com/vertbone/entry/verticles/EntryVerticle.java
@@ -0,0 +1,21 @@
+package com.vertbone.entry.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by vasiliy on 31.12.16.
+ */
+@Component
+public class EntryVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+}
