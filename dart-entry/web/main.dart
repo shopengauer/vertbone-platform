1392bbcc491e1667685fdc207059747538936e54
root <localducks@gmail.com>
2017-01-05 22:22:05 +0300

    Add button to dart file!!!

--- dart-entry/web/main.dart
+++ dart-entry/web/main.dart
@@ -5,4 +5,12 @@ import 'dart:html';
 
 void main() {
   querySelector('#output').text = 'Dart Waider here!!! Dart is WORK!!!';
+
+  var button = new Element.tag('button');
+  button.text = 'Click Dart';
+  document.body.children.add(button);
+
+  var button2 = new Element.tag('button');
+  button2.text = 'Button 2';
+  document.body.children.add(button2);
 }
