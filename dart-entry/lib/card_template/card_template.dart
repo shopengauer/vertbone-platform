84669705023c4a14d5c9ffd2560982608c51d5a0
root <localducks@gmail.com>
2017-02-15 22:42:40 +0300

    Добавил шаблоны карточек

--- dart-entry/lib/card_template/card_template.dart
+++ dart-entry/lib/card_template/card_template.dart
@@ -1,7 +1,11 @@
 import 'package:angular2/core.dart';
 
 
-
+@Component(
+ selector: 'card-template',
+ templateUrl: 'card_template.html',
+ styleUrls: const ['card_template.css'],
+)
 class CardTemplate{
 
 
--- dart-entry/lib/content_layout/content_layout.dart
+++ dart-entry/lib/content_layout/content_layout.dart
@@ -1,9 +1,10 @@
 import 'package:angular2/core.dart';
-
+import 'package:dart_entry/card_template/card_template.dart';
 
 @Component(
   selector: 'content',
-  templateUrl: 'content_layout.html'
+  templateUrl: 'content_layout.html',
+  directives: const [CardTemplate]
 )
 class ContentLayout{
 
